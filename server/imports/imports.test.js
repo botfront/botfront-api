1d27eb935b3d2b7a165230a43dc7bd25d83ea8e9
Guillaume TECHER <guillaumeval.techer@gmail.com>
2019-10-09 15:34:23 -0400

    feat: test update for import route

--- server/imports/imports.test.js
+++ server/imports/imports.test.js
@@ -4,7 +4,17 @@ const chai = require('chai');
 const expect = chai.expect;
 const app = require('../../app');
 chai.config.includeStack = true;
-const { Projects, NLUModels, Conversations } = require('../../models/models');
+const fs = require('fs');
+const {
+  Projects,
+  NLUModels,
+  Conversations,
+  Activity
+} = require('../../models/models');
+
+const conversationsToImport = JSON.parse(
+  fs.readFileSync(__dirname + '/test_data/conversationsToImport.json', 'utf8')
+);
 
 function dateParser(key, value) {
   if (key === 'updatedAt' || key === 'createdAt') {
@@ -13,7 +23,6 @@ function dateParser(key, value) {
   return value;
 }
 before(function(done) {
-  const fs = require('fs');
   const projectsFile = __dirname + '/test_data/projects.json';
   const modelsFile = __dirname + '/test_data/nluModels.json';
   const conversationFile = __dirname + '/test_data/conversations.json';
@@ -23,6 +32,7 @@ before(function(done) {
     fs.readFileSync(conversationFile, 'utf8'),
     dateParser
   );
+
   Projects.insertMany(projects)
     .then(() => NLUModels.insertMany(models))
     .then(() => Conversations.insertMany(conversation))
@@ -82,6 +92,147 @@ describe('## last import', () => {
               'environement should be one of: production, staging, developement'
           });
           done();
+        })
+        .catch(done);
+    });
+  });
+});
+
+describe('## import format checking', () => {
+  describe('# POST /conversations/environment/{env}', () => {
+    it('should fail with invalid body', done => {
+      request(app)
+        .post('/conversations/environment/production')
+        .send({
+          dummy: [{ name: 'test', confidence: 0.99 }],
+          text: 'blabla'
+        })
+        .expect(httpStatus.BAD_REQUEST)
+        .then(res => {
+          expect(res.body).to.deep.equal({
+            error: 'the body is missing conversations or processNlu, or both'
+          });
+          done();
+        })
+        .catch(done);
+    });
+    it('should fail with invalid conversations type', done => {
+      request(app)
+        .post('/conversations/environment/production')
+        .send({
+          conversations: 'bad',
+          processNlu: false
+        })
+        .expect(httpStatus.BAD_REQUEST)
+        .then(res => {
+          expect(res.body).to.deep.equal({
+            error: 'conversations should be an array'
+          });
+          done();
+        })
+        .catch(done);
+    });
+    it('should fail with invalid processNlu type', done => {
+      request(app)
+        .post('/conversations/environment/production')
+        .send({
+          conversations: [],
+          processNlu: 'false'
+        })
+        .expect(httpStatus.BAD_REQUEST)
+        .then(res => {
+          expect(res.body).to.deep.equal({
+            error: 'processNlu should be an boolean'
+          });
+          done();
+        })
+        .catch(done);
+    });
+    it('should import a new conversation and update and oldOne', done => {
+      request(app)
+        .post('/conversations/environment/production')
+        .send({
+          conversations: conversationsToImport.slice(0, 2),
+          processNlu: true
+        })
+        .expect(httpStatus.OK)
+        .then(async res => {
+          expect(res.body).to.deep.equal({
+            message: 'successfuly imported all conversations'
+          });
+          Conversations.find({ _id: 'new' })
+            .lean()
+            .exec()
+            .then(newData => {
+              expect(newData).to.have.length(1);
+              Conversations.find({ _id: 'update' })
+                .lean()
+                .exec()
+                .then(updateData => {
+                  expect(updateData).to.have.length(1);
+                  expect(updateData[0].updatedAt).to.not.equal(
+                    new Date(1550000000)
+                  );
+                  Activity.find({ text: 'newevent' })
+                    .lean()
+                    .exec()
+                    .then(activityData => {
+                      expect(activityData).to.have.length(1);
+                      done();
+                    });
+                });
+            })
+            .catch(done);
+        });
+    });
+
+    it('should not import a conversation with a non existing project id', done => {
+      request(app)
+        .post('/conversations/environment/production')
+        .send({
+          conversations: conversationsToImport.slice(2, 3),
+          processNlu: true
+        })
+        .expect(httpStatus.PARTIAL_CONTENT)
+        .then(async res => {
+          expect(res.body).to.deep.equal({
+            messageConversation:
+              'some conversation were not added, either the _id is missing or projectId does not exist',
+            notValids: [conversationsToImport[2]]
+          });
+          Conversations.find({ _id: 'projectnotexist' })
+            .lean()
+            .exec()
+            .then(newData => {
+              expect(newData).to.have.length(0);
+              done();
+            });
+        })
+        .catch(done);
+    });
+    it('should not import a wrong parse data with a non existing project id', done => {
+      request(app)
+        .post('/conversations/environment/production')
+        .send({
+          conversations: conversationsToImport.slice(3),
+          processNlu: true
+        })
+        .expect(httpStatus.PARTIAL_CONTENT)
+        .then(async res => {
+          expect(res.body).to.deep.equal({
+            messageParseData:
+              'Some parseData have not been added to activity, the corresponding models could not be found ',
+            invalidParseDatas: [
+              [conversationsToImport[3].tracker.events[6].parse_data]
+            ]
+          });
+          Conversations.find({ _id: 'projectnotexist' })
+            .lean()
+            .exec()
+            .then(newData => {
+              expect(newData).to.have.length(0);
+              done();
+            });
         })
         .catch(done);
     });
