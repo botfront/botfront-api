56a3755a61c7b1cbc931ff96f4be63d28a138062
Guillaume TECHER <guillaumeval.techer@gmail.com>
2019-10-09 15:33:57 -0400

    fix: modification following the tests for the import route

--- server/imports/imports.controller.js
+++ server/imports/imports.controller.js
@@ -10,18 +10,19 @@ retreiveProjectsAndModelsIds = async function() {
     .select('nlu_models _id')
     .lean()
     .exec();
-
-  projectAndModels = projects.map(async project => {
-    nluModels = await project.nlu_models.map(async projectNluModel => {
-      nluModel = NLUModels.findOne({ _id: projectNluModel })
-        .select('_id language')
-        .lean()
-        .exec();
-      return nluModel;
+  projectAndModels = projects
+    .filter(project => project.nlu_models !== undefined)
+    .map(async project => {
+      nluModels = await project.nlu_models.map(async projectNluModel => {
+        nluModel = NLUModels.findOne({ _id: projectNluModel })
+          .select('_id language')
+          .lean()
+          .exec();
+        return nluModel;
+      });
+      nluModels = await Promise.all(nluModels);
+      return { projectId: project._id, nluModels: nluModels };
     });
-    nluModels = await Promise.all(nluModels);
-    return { projectId: project._id, nluModels: nluModels };
-  });
   return await Promise.all(projectAndModels);
 };
 
@@ -32,7 +33,8 @@ inferModelId = function(projectId, language, projectsAndModels) {
   const modelId = project.nluModels.find(
     nluModel => nluModel.language === language
   );
-  return modelId._id;
+  if (modelId) return modelId._id;
+  return undefined;
 };
 
 addParseDataToActivity = async function(conversation, oldestImportTimestamp) {
@@ -44,6 +46,7 @@ addParseDataToActivity = async function(conversation, oldestImportTimestamp) {
     if (
       event.parse_data !== undefined &&
       event.parse_data.language !== undefined &&
+      event.parse_data.text !== '' &&
       event.timestamp > oldestImportTimestamp
     ) {
       const { intent, entities, text } = event.parse_data;
@@ -54,8 +57,8 @@ addParseDataToActivity = async function(conversation, oldestImportTimestamp) {
       );
       if (modelId) {
         parseDataToAdd.push({
-          modelId,
-          text,
+          modelId: modelId,
+          text: text,
           intent: intent.name,
           entities,
           confidence: intent.confidence,
@@ -72,7 +75,7 @@ addParseDataToActivity = async function(conversation, oldestImportTimestamp) {
 
 createConversationsToAdd = function(conversations, env, projectsIds) {
   const toAdd = [];
-  const invalids = [];
+  const notValids = [];
   conversations.forEach(conversation => {
     if (
       conversation._id !== undefined &&
@@ -85,7 +88,7 @@ createConversationsToAdd = function(conversations, env, projectsIds) {
         createdAt: new Date(conversation.createdAt)
       });
     } else {
-      invalids.push(conversation);
+      notValids.push(conversation);
     }
   });
 
@@ -99,6 +102,11 @@ exports.importConversation = async function(req, res) {
   if (!['production', 'staging', 'developement'].includes(env)) {
     return res.status(400).json({
       error: 'environement should be one of: production, staging, developement'
+    });
+  }
+  if (conversations === undefined || processNlu === undefined) {
+    return res.status(400).json({
+      error: 'the body is missing conversations or processNlu, or both'
     });
   }
   if (!Array.isArray(conversations)) {
@@ -123,41 +131,53 @@ exports.importConversation = async function(req, res) {
 
   //delacred out of the forEach Block so it can be accessed later
   const invalidParseDatas = [];
-  // add each prepared conversatin to the db
-  toAdd.forEach(conversation => {
-    Conversations.updateOne(
-      { _id: conversation._id },
-      conversation,
-      { upsert: true },
-      function(err) {
-        if (err) return res.status(400).json(err);
-      }
-    );
-    if (processNlu) {
-      const { parseDataToAdd, invalidParseData } = addParseDataToActivity(
+  // add each prepared conversatin to the db, a promise all is used to ensure that all data is added before checking for errors
+  errors = [];
+  await Promise.all(
+    toAdd.map(async conversation => {
+      Conversations.updateOne(
+        { _id: conversation._id },
         conversation,
-        oldestImport
+        { upsert: true },
+        function(err) {
+          if (err) errors.push(err);
+        }
       );
-      Activity.insertMany(parseDataToAdd);
-      if (invalidParseData.length > 0) invalidParseDatas.push(invalidParseData);
-    }
-  });
+      if (processNlu) {
+        const {
+          parseDataToAdd,
+          invalidParseData
+        } = await addParseDataToActivity(conversation, oldestImport);
+        if (parseDataToAdd && parseDataToAdd.length > 0) {
+          await Activity.insertMany(parseDataToAdd, function(err) {
+            if (err) errors.push(err);
+          });
+        }
+        if (invalidParseData && invalidParseData.length > 0)
+          invalidParseDatas.push(invalidParseData);
+      }
+    })
+  );
+
+  if (errors.length > 0) {
+    return res.status(500).json(errors);
+  }
 
   //create a report of the errors, if any
-  const error = {};
-  if (notValids.length > 0) {
-    error.messageConversation =
+  const formatsError = {};
+  if (notValids && notValids.length > 0) {
+    formatsError.messageConversation =
       'some conversation were not added, either the _id is missing or projectId does not exist';
-    error.notValids = notValids;
+    formatsError.notValids = notValids;
   }
   if (invalidParseDatas.length > 0) {
-    error.messageParseData =
+    formatsError.messageParseData =
       'Some parseData have not been added to activity, the corresponding models could not be found ';
-    error.invalidParseDatas = invalidParseDatas;
+    formatsError.invalidParseDatas = invalidParseDatas;
   }
   //object not empty
-  if (Object.keys(error).length !== 0) {
-    return res.status(206).json(error);
+  if (Object.keys(formatsError).length !== 0) {
+    return res.status(206).json(formatsError);
   }
 
   return res
